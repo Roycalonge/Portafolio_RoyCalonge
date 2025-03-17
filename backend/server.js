1fa6b52c6b974d0642df0c9be836f9d71e96ed76
RoyCalonge <roycalonge@hotmail.com>
2025-03-17 18:50:46 -0500

    Subiendo proyecto inicial con frontend y backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 // backend/server.js
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Habilitar CORS
+app.use(cors());
 
 // Servir archivos estáticos del frontend
 app.use(express.static(path.join(__dirname, '../frontend')));
