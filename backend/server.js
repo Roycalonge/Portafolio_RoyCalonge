78ff2617e8fdef26f3c7bd86550e6c8a32ed7f3b
RoyCalonge <roycalonge@hotmail.com>
2025-03-19 20:48:22 -0500

    Actualizando render y git

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,6 @@ app.get('/web-design', (req, res) => {
 });
 
 // Iniciar el servidor
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
